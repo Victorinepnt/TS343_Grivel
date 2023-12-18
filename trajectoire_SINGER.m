e3acc653cdeae93c6cb5d383cf6dafe03aa4b5f8
Victorine Penot <vpenot001@bordeaux-inp.fr>
2023-12-18 09:46:30 +0100

    modif SINGER

--- trajectoire_SINGER.m
+++ trajectoire_SINGER.m
@@ -3,10 +3,10 @@ close all,
 clc,
 
 %% Définition des paramètres 
-nb_ech = 100;
-x = zeros(1,nb_ech);
-y = zeros(1,nb_ech);
-bruit_modele = randn(1,nb_ech);
+N = 100;
+x = zeros(1,N);
+y = zeros(1,N);
+bruit_modele = randn(1,N);
 
 
 
